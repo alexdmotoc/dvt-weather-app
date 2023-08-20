cf85a81b12dd51e0db4b53adf1eae7a4f2e3064d
Alex Motoc <alexdmotoc@gmail.com>
2023-08-20 20:36:18 +0300

    move HTTPClient implementation to corresponding module

--- WeatherApp/Data/Network/HTTPClient.swift
+++ WeatherApp/Data/Network/HTTPClient.swift
@@ -10,3 +10,25 @@ import Foundation
 public protocol HTTPClient {
     func load(urlReqeust: URLRequest) async throws -> (Data, HTTPURLResponse)
 }
+
+// MARK: - Implementation
+
+public final class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    private enum Error: Swift.Error {
+        case invalidResponse
+    }
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    public func load(urlReqeust: URLRequest) async throws -> (Data, HTTPURLResponse) {
+        let (data, response) = try await session.data(for: urlReqeust)
+        guard let response = response as? HTTPURLResponse else {
+            throw Error.invalidResponse
+        }
+        return (data, response)
+    }
+}
--- WeatherAppTests/Data/Network/URLSessionHTTPClientTests.swift
+++ WeatherAppTests/Data/Network/URLSessionHTTPClientTests.swift
@@ -8,26 +8,6 @@
 import XCTest
 import WeatherApp
 
-public final class URLSessionHTTPClient: HTTPClient {
-    private let session: URLSession
-    
-    private enum Error: Swift.Error {
-        case invalidResponse
-    }
-    
-    public init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
-    public func load(urlReqeust: URLRequest) async throws -> (Data, HTTPURLResponse) {
-        let (data, response) = try await session.data(for: urlReqeust)
-        guard let response = response as? HTTPURLResponse else {
-            throw Error.invalidResponse
-        }
-        return (data, response)
-    }
-}
-
 class URLSessionHTTPClientTests: XCTestCase {
     
     override func setUp() {
