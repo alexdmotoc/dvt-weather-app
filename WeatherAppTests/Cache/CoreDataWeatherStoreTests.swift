edb2b63f6d0e6991708f8fe2a8293c19e2063500
Alex Motoc <alexdmotoc@gmail.com>
2023-08-21 21:06:54 +0300

    add core data weather store tests

---
+++ WeatherAppTests/Cache/CoreDataWeatherStoreTests.swift
@@ -0,0 +1,73 @@
+//
+//  CoreDataWeatherStoreTests.swift
+//  WeatherAppTests
+//
+//  Created by Alex Motoc on 21.08.2023.
+//
+
+import XCTest
+import WeatherApp
+
+class CoreDataWeatherStoreTests: XCTestCase {
+    
+    func test_load_returnsEmptyOnEmptyStore() throws {
+        let sut = makeSUT()
+        
+        let items = try sut.load()
+        
+        XCTAssertTrue(items.isEmpty)
+    }
+    
+    func test_save_returnsNoError() throws {
+        let sut = makeSUT()
+        let itemsToSave = (0 ..< 5).map { _ in makeWeatherInformation() }
+        
+        try sut.save(itemsToSave)
+    }
+    
+    func test_save_returnsSavedItems() throws {
+        let sut = makeSUT()
+        let itemsToSave = (0 ..< 5).map { _ in makeWeatherInformation() }
+        
+        try sut.save(itemsToSave)
+        let savedItems = try sut.load()
+        
+        XCTAssertEqual(itemsToSave, savedItems)
+    }
+    
+    func test_deleteAllItems_returnsNoErrorOnEmptyStore() throws {
+        let sut = makeSUT()
+        try sut.deleteAllItems()
+    }
+    
+    func test_deleteAllItems_emptiesStore() throws {
+        let sut = makeSUT()
+        let itemsToSave = (0 ..< 5).map { _ in makeWeatherInformation() }
+        
+        try sut.save(itemsToSave)
+        var savedItems = try sut.load()
+        XCTAssertEqual(itemsToSave, savedItems)
+        
+        try sut.deleteAllItems()
+        savedItems = try sut.load()
+        XCTAssertEqual(savedItems, [])
+    }
+    
+    // MARK: - Helpers
+    
+    func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> WeatherStore {
+        let url = URL(fileURLWithPath: "/dev/null")
+        let store = try! CoreDataWeatherStore(storeURL: url)
+        checkIsDeallocated(sut: store, file: file, line: line)
+        return store
+    }
+    
+    func makeForecast() -> WeatherInformation.Forecast {
+        .init(currentTemp: 123, weatherType: .init(rawValue: Int.random(in: 0 ... 2)) ?? .sunny)
+    }
+    
+    func makeWeatherInformation() -> WeatherInformation {
+        let forecast = (0 ..< 5).map { _ in makeForecast() }
+        return makeWeatherInformation(locationName: "mock", forecast: forecast)
+    }
+}
