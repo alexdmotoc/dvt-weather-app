c13a048ae5d38f4cbf85ed6da3d1381064c8c4a1
Alex Motoc <alexdmotoc@gmail.com>
2023-08-22 08:37:20 +0300

    move WeatherRepository implementation to corresponding module

--- WeatherApp/Data/WeatherRepository.swift
+++ WeatherApp/Data/WeatherRepository.swift
@@ -12,3 +12,42 @@ public protocol WeatherRepository {
     func getWeather(cacheHandler: ([WeatherInformation]) -> Void) async throws -> [WeatherInformation]
     func addFavouriteLocation(coordinates: CLLocationCoordinate2D) async throws -> WeatherInformation
 }
+
+// MARK: - Implementation
+
+public final class WeatherRepositoryImpl: WeatherRepository {
+    
+    private let fetcher: RemoteWeatherFetcher
+    private let cache: WeatherCache
+    private let currentLocation: () -> CLLocationCoordinate2D
+    
+    public init(fetcher: RemoteWeatherFetcher, cache: WeatherCache, currentLocation: @escaping () -> CLLocationCoordinate2D) {
+        self.fetcher = fetcher
+        self.cache = cache
+        self.currentLocation = currentLocation
+    }
+    
+    public func getWeather(cacheHandler: ([WeatherInformation]) -> Void) async throws -> [WeatherInformation] {
+        let weatherCache = try cache.load()
+        cacheHandler(weatherCache)
+        
+        let currentWeather = try await fetcher.fetch(coordinates: currentLocation(), isCurrentLocation: true)
+        var results = [currentWeather]
+        for favouriteWeather in weatherCache.filter({ !$0.isCurrentLocation }) {
+            let updatedFavouriteWeather = try await fetcher.fetch(coordinates: favouriteWeather.location.coordinates, isCurrentLocation: false)
+            results.append(updatedFavouriteWeather)
+        }
+        
+        try cache.save(results)
+        
+        return results
+    }
+    
+    public func addFavouriteLocation(coordinates: CLLocationCoordinate2D) async throws -> WeatherInformation {
+        let weather = try await fetcher.fetch(coordinates: coordinates, isCurrentLocation: false)
+        var cached = try cache.load()
+        cached.append(weather)
+        try cache.save(cached)
+        return weather
+    }
+}
--- WeatherAppTests/Data/WeatherRepositoryTests.swift
+++ WeatherAppTests/Data/WeatherRepositoryTests.swift
@@ -9,43 +9,6 @@ import XCTest
 import WeatherApp
 import CoreLocation
 
-final class WeatherRepositoryImpl: WeatherRepository {
-    
-    private let fetcher: RemoteWeatherFetcher
-    private let cache: WeatherCache
-    private let currentLocation: () -> CLLocationCoordinate2D
-    
-    init(fetcher: RemoteWeatherFetcher, cache: WeatherCache, currentLocation: @escaping () -> CLLocationCoordinate2D) {
-        self.fetcher = fetcher
-        self.cache = cache
-        self.currentLocation = currentLocation
-    }
-    
-    func getWeather(cacheHandler: ([WeatherInformation]) -> Void) async throws -> [WeatherInformation] {
-        let weatherCache = try cache.load()
-        cacheHandler(weatherCache)
-        
-        let currentWeather = try await fetcher.fetch(coordinates: currentLocation(), isCurrentLocation: true)
-        var results = [currentWeather]
-        for favouriteWeather in weatherCache.filter({ !$0.isCurrentLocation }) {
-            let updatedFavouriteWeather = try await fetcher.fetch(coordinates: favouriteWeather.location.coordinates, isCurrentLocation: false)
-            results.append(updatedFavouriteWeather)
-        }
-        
-        try cache.save(results)
-        
-        return results
-    }
-    
-    func addFavouriteLocation(coordinates: CLLocationCoordinate2D) async throws -> WeatherInformation {
-        let weather = try await fetcher.fetch(coordinates: coordinates, isCurrentLocation: false)
-        var cached = try cache.load()
-        cached.append(weather)
-        try cache.save(cached)
-        return weather
-    }
-}
-
 class WeatherRepositoryTests: XCTestCase {
     func test_init_doesntProduceSideEffects() {
         let (fetcher, cache, _) = makeSUT()
