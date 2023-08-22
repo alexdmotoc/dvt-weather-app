4290f96cfa0c044968488fd458044826ed868005
Alex Motoc <alexdmotoc@gmail.com>
2023-08-22 16:58:24 +0300

    iOS app init

---
+++ WeatherApp-iOS/WeatherApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_iOSApp.swift
+//  WeatherApp-iOS
+//
+//  Created by Alex Motoc on 22.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
