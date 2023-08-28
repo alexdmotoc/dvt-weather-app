21851cfd98fdd9a5a1d2c764ca74ee31b1c2d33a
Alex Motoc <alexdmotoc@gmail.com>
2023-08-28 21:36:58 +0300

    move data types into their corresponding module

--- WeatherApp/Data/RemotePlaceDetailsFetcher.swift
+++ WeatherApp/Data/RemotePlaceDetailsFetcher.swift
@@ -10,3 +10,69 @@ import Foundation
 public protocol RemotePlaceDetailsFetcher {
     func fetchDetails(placeName: String) async throws -> PlaceDetails
 }
+
+// MARK: - Implementation
+
+public class RemotePlaceDetailsFetcherImpl: RemotePlaceDetailsFetcher {
+    
+    private let client: HTTPClient
+    
+    public init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    public func fetchDetails(placeName: String) async throws -> PlaceDetails {
+        let response = try await fetchPlace(name: placeName)
+        guard let placeId = response.results.first?.place_id else { throw Error.placeNotFound }
+        let details = try await fetchPlaceDetails(placeId: placeId)
+        return details.toLocal
+    }
+    
+    private func fetchPlace(name: String) async throws -> PlaceResponseDTO {
+        let request = try PlacesAPIURLRequestFactory.makeGetPlaceURLRequest(query: name)
+        let (data, response) = try await client.load(urlReqeust: request)
+        return try DataMapper.map(data: data, response: response)
+    }
+    
+    private func fetchPlaceDetails(placeId: String) async throws -> PlaceDetailsDTO {
+        let request = try PlacesAPIURLRequestFactory.makeGetPlaceDetailsURLRequest(placeId: placeId)
+        let (data, response) = try await client.load(urlReqeust: request)
+        return try DataMapper.map(data: data, response: response)
+    }
+    
+    // MARK: - Error
+    
+    public enum Error: Swift.Error, LocalizedError {
+        case invalidData
+        case placeNotFound
+        
+        public var errorDescription: String? {
+            switch self {
+            case .invalidData:
+                return NSLocalizedString(
+                    "api.error.message",
+                    bundle: Bundle(for: RemotePlaceDetailsFetcherImpl.self),
+                    comment: ""
+                )
+            case .placeNotFound:
+                return NSLocalizedString(
+                    "api.places.error.notFound",
+                    bundle: Bundle(for: RemotePlaceDetailsFetcherImpl.self),
+                    comment: ""
+                )
+            }
+        }
+    }
+    
+    // MARK: - DataMapper
+    
+    private enum DataMapper {
+        static func map<T: Decodable>(data: Data, response: HTTPURLResponse) throws -> T {
+            guard
+                response.statusCode == 200,
+                let mapped = try? JSONDecoder().decode(T.self, from: data)
+            else { throw Error.invalidData }
+            return mapped
+        }
+    }
+}
